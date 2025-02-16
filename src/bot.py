cc36c08b57bcfdbfd514f4317a816c4b4c6b7c3f
blandk77 <alexags1983@gmail.com>
2025-02-17 00:00:47 +0530

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -165,7 +165,7 @@ async def check_daily_limit(user_id: int, file_size: int) -> bool:
 
 
 if __name__ == '__main__':
-    register_commands(bot, tasks, stop_download, zip_names, STORAGE, DAILY_LIMIT_GB, IS_PREMIUM, user_collection, PREMIUM_DAYS, PAID_PLANS, UPI_DETAILS, ADMIN_USER_ID, FILES_CHANNEL, check_daily_limit, get_running_loop, rmtree, download_files, add_to_zip, logging)
+    register_commands(bot, tasks, stop_download, zip_names, STORAGE, DAILY_LIMIT_GB, IS_PREMIUM, user_collection, PREMIUM_DAYS, PAID_PLANS, UPI_DETAILS, ADMIN_USER_ID, FILES_CHANNEL, check_daily_limit, get_running_loop, rmtree, download_files, add_to_zip, logging, is_premium_user, add_premium_user, get_daily_usage, set_daily_usage, bot.get_entity) #Added functions and bot.get_entity
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_start_message())
     bot.run_until_disconnected()

167adf8f2e0374c2bef6e4725a7854a9d1064e62
blandk77 <alexags1983@gmail.com>
2025-02-17 23:17:04 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,37 +3,50 @@ import time
 from pathlib import Path
 from zipfile import ZipFile
 
-async def download_files(message, root, bot, event, progress_message_id, total_files, file_number, start_time, total_size):
+from pyrofork import Client
+from pyrofork.types import Message
+
+async def download_files(message: Message, root: Path, client: Client, progress_message, total_files, file_number, start_time, total_size):
     try:
-        file_path = root / message.file.name if message.file.name else root / str(message.id)
+        # Determine file name
+        if message.media.document and message.media.document.file_name:
+            file_name = message.media.document.file_name
+        elif message.media.photo:
+            file_name = f"photo_{message.id}.jpg"  # Or a more appropriate extension
+        else:
+            file_name = str(message.id)  # Fallback to message ID
+
+        file_path = root / file_name
         file_path_str = str(file_path)
         start_dl_time = time.time()
-        async def callback(downloaded, total):
+
+        async def callback(current, total):
             nonlocal start_dl_time
             current_time = time.time()
             time_taken = current_time - start_dl_time
-            speed = downloaded / time_taken if time_taken > 0 else 0
+            speed = current / time_taken if time_taken > 0 else 0
             speed_mbps = speed / (1024 * 1024)
             remaining_files = total_files - file_number
 
             minutes = int((current_time - start_time) // 60)
             seconds = int((current_time - start_time) % 60)
 
-
-            progress_message = (
+            progress_message_text = (
                 "========Status========\n"
                 "Downloading File: {}/{}/{}\n"
                 "Time Taken: {}m {}s\n"
                 "Speed: {:.2f} MB/s\n"
                 "To stop the process, use /stop"
-            ).format(file_number, total_files,remaining_files, minutes, seconds, speed_mbps)
+            ).format(file_number, total_files, remaining_files, minutes, seconds, speed_mbps)
+
             try:
-                await bot.edit_message(event.chat_id, progress_message_id, progress_message, parse_mode='html')
+                await progress_message.edit_text(progress_message_text)
             except Exception as e:
                 print(f"Error editing message: {e}")
                 return
 
-        await bot.download_media(message, file=file_path_str, progress_callback=callback)
+        # Download the file
+        await client.download_media(message, file=file_path_str, progress=callback)
         return file_path
     except Exception as e:
         print(f"Error downloading file: {e}")

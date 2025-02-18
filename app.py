120963bd6e641d8efeeb5a8b7914aeb987c90edf
blandk77 <alexags1983@gmail.com>
2025-02-19 00:10:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,16 +158,6 @@ async def check_daily_limit(user_id: int, file_size: int) -> bool:
     else:
         return False
 
-# Start the bot within the Flask app's event loop
-loop = asyncio.get_event_loop()
-async def start_bot():
-    try:
-        await bot.start()
-        await send_start_message(bot)
-        logging.info("Telegram bot started")
-    except Exception as e:
-        logging.error(f"Error starting Telegram bot: {e}")
-
 # Register commands
 register_commands(bot, tasks, stop_download, zip_names, STORAGE, DAILY_LIMIT_GB, IS_PREMIUM,
                   user_collection, PREMIUM_DAYS, PAID_PLANS, UPI_DETAILS, ADMIN_USER_ID,
@@ -175,9 +165,18 @@ register_commands(bot, tasks, stop_download, zip_names, STORAGE, DAILY_LIMIT_GB,
                   download_files, add_to_zip, logging, is_premium_user, add_premium_user,
                   get_daily_usage, set_daily_usage)
 
+# Start the bot within the Flask app's event loop
+async def main():
+    try:
+        await bot.start()
+        await send_start_message(bot)
+        logging.info("Telegram bot started")
+    except Exception as e:
+        logging.error(f"Error starting Telegram bot: {e}")
+
 @app.before_first_request
 def before_first_request():
-    loop.create_task(start_bot())
+    asyncio.run(main())
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
